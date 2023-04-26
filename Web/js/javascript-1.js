da3e3e5ff5e361c5938f22dcb35d3b75c062f7e1
Alex-Barr0n <a01783126@tec.mx>
2023-04-26 11:59:20 -0600

    Changed java

--- Web/js/javascript-1.js
+++ Web/js/javascript-1.js
@@ -5,18 +5,27 @@ Prueba tu función con: 'abacddbec'
 
 function repetido(str) 
 {
-    const contador = {}; 
+  const contador = {}; 
     
-    for (let caracter of str) {
-      contador[caracter] = (contador[caracter] || 0) + 1;
-    }
+  for (let caracter of str) {
+    contador[caracter] = (contador[caracter] || 0) + 1;
+  }
     
-    for (let caracter of str) {
-      if (contador[caracter] == 1) {
-        return caracter;
-      }
+  for (let caracter of str) {
+    if (contador[caracter] == 1) {
+      return caracter;
     }
+  } 
 }
 console.log("En la cabena de caracteres 'abacddbec' la primera letra que no se repite es 'e'" + repetido("abacddbec"))
 
 //Escribe una función que implemente el algoritmo 'bubble-sort' para ordenar una lista de números.
+
+function bubble_sort(lst)
+{
+  const lgnth = lst.length;
+  for (let i=0; i < lgnth; i++)
+  {
+    for (let j=0; j < lgnth-1; j++)
+  }
+}
