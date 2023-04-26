e123c1d4e6aa9fbce7b14f1ef8ab7cd26db2a5de
Alex-Barr0n <a01783126@tec.mx>
2023-04-26 12:44:25 -0600

    Changed javascript js

--- Web/js/javascript-1.js
+++ Web/js/javascript-1.js
@@ -19,13 +19,36 @@ function repetido(str)
 }
 console.log("En la cabena de caracteres 'abacddbec' la primera letra que no se repite es 'e'" + repetido("abacddbec"))
 
-//Escribe una función que implemente el algoritmo 'bubble-sort' para ordenar una lista de números.
+//Escribe una función que implemente el algoritmo 'bubble-sort' para ordenar una lst de números.
 
 function bubble_sort(lst)
 {
   const lgnth = lst.length;
-  for (let i=0; i < lgnth; i++)
+  for (let i=0; i < lgnth-1; i++)
   {
-    for (let j=0; j < lgnth-1; j++)
+    for (let j=0; j < lgnth-i-1; j++)
+    {
+      if (lst[j] > lst[j + 1])
+      {
+      const temp = lst[j]; 
+      lst[j] = lst[j + 1];
+      lst[j + 1] = temp;
+      }
+    }
   }
+}
+
+/*
+Escribe dos funciones: la primera que invierta un arreglo de números y regrese un nuevo arreglo con el resultado; 
+la segunda que modifique el mismo arreglo que se pasa como argumento. No se permite usar la función integrada 'reverse'.
+*/
+
+function invert(arreglo)
+{
+
+}
+
+function modify(arreglo)
+{
+  
 }
